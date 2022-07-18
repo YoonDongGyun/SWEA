362fa1fb3fe7b39bd9b8da1f869efe40e4969da0
YoonDongGyun <ydg8732@naver.com>
2022-07-18 21:44:37 +0900

    D1_2068_최대수 구하기

---
+++ src/d1/d1_2068.java
@@ -0,0 +1,28 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_2068 {
+
+	public static void main(String[] args) {
+		int T;
+		int[] arr = new int[10];
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		T=sc.nextInt();
+		
+		for(int i=1;i<=T;i++) {
+			int max=Integer.MIN_VALUE;
+			for(int j=0;j<10;j++) {
+				arr[j]=sc.nextInt();
+				if(max<arr[j]) max=arr[j];
+			}
+			
+			System.out.println("#"+i+" "+max);
+		}
+
+	}
+
+}
