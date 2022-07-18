4684933dc531a481d15d0e649c577faa60599ab1
YoonDongGyun <ydg8732@naver.com>
2022-07-18 21:10:37 +0900

    D1_2043_서랍의 비밀번호

---
+++ src/d1/d1_2043.java
@@ -0,0 +1,19 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_2043 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int P,K;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		P=sc.nextInt();
+		K=sc.nextInt();
+		
+		System.out.println(P-K+1);
+	}
+
+}
