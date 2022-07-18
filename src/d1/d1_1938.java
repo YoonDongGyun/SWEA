262c34a12232692bc2f5771ac55d2c3356413b8b
YoonDongGyun <ydg8732@naver.com>
2022-07-18 18:59:43 +0900

    D1_1938_아주 간단한 계산기

---
+++ src/d1/d1_1938.java
@@ -0,0 +1,23 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_1938 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		
+	}
+
+}
