7bea4dfaa34d1496a4927ad4172008cb2cce320a
YoonDongGyun <ydg8732@naver.com>
2022-07-18 21:19:55 +0900

    D1_1936_1대1 가위바위보

---
+++ src/d1/d1_1936.java
@@ -0,0 +1,33 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_1936 {
+
+	public static void main(String[] args) {
+		int A,B;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		A=sc.nextInt();
+		B=sc.nextInt();
+		
+		switch(A) {
+		case 1:
+			if(B==2) System.out.println("B");
+			else if (B==3) System.out.println("A");
+			break;
+			
+		case 2:
+			if(B==1) System.out.println("A");
+			else if (B==3) System.out.println("B");
+			break;
+			
+		case 3:
+			if(B==1) System.out.println("B");
+			else if(B==2)System.out.println("A");
+			break;
+		}
+	}
+
+}
