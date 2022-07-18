8f917d801e74e0673e1c9f9fe1e81a20bc0fabf2
YoonDongGyun <ydg8732@naver.com>
2022-07-18 20:53:47 +0900

    D1_2047_신문 헤드라인

---
+++ src/d1/d1_2047.java
@@ -0,0 +1,19 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_2047 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String head_line;
+		Scanner sc = new Scanner(System.in);
+		
+		head_line=sc.next();
+		
+		head_line=head_line.toUpperCase();
+		
+		System.out.println(head_line);
+	}
+
+}
