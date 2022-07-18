4287c9f980fd592a207d6a5c8c87633b71daedf5
YoonDongGyun <ydg8732@naver.com>
2022-07-18 20:28:00 +0900

    D1_2046_스탬프 찍기

---
+++ src/d1/d1_2046.java
@@ -0,0 +1,20 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_2046 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		num=sc.nextInt();
+		
+		for (int i=0;i<num;i++) {
+			System.out.printf("#");
+		}
+	}
+
+}
