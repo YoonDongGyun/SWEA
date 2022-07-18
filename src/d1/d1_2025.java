14ec3bab96eca300fd1e47e5c0b7dbf7b584a3c1
YoonDongGyun <ydg8732@naver.com>
2022-07-18 21:02:28 +0900

    D1_2025_N줄덧셈

---
+++ src/d1/d1_2025.java
@@ -0,0 +1,20 @@
+package d1;
+
+import java.util.Scanner;
+
+public class d1_2025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num,sum=0;
+		Scanner sc = new Scanner(System.in);
+		
+		num=sc.nextInt();
+		for(int i=1;i<=num;i++) {
+			sum+=i;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
